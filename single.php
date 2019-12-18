35e98dd3b6b61da58515290043045bc4a9a9bfe7
Khalil <f6apps@gmail.com>
2019-12-18 16:57:07 -0600

    Fictional University Files

---
+++ single.php
@@ -0,0 +1,83 @@
+<?php
+ get_header();
+
+while(have_posts()) {
+  the_post(); ?>
+
+
+
+<header class="site-header">
+    <div class="container">
+      <h1 class="school-logo-text float-left"><a href="<?php get_the_permalink(); ?>"><strong>Fictional</strong> University</a></h1>
+      <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+      <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
+      <div class="site-header__menu group">
+        <nav class="main-navigation">
+          <ul>
+            <li class="current-menu-item"><a href="#">About Us</a></li>
+            <li><a href="#">Programs</a></li>
+            <li><a href="#">Events</a></li>
+            <li><a href="#">Campuses</a></li>
+            <li><a href="#">Blog</a></li>
+          </ul>
+        </nav>
+        <div class="site-header__util">
+          <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
+          <a href="#" class="btn btn--small  btn--dark-orange float-left">Sign Up</a>
+          <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title"><?php the_title(); ?></h1>
+      <div class="page-banner__intro">
+        <p>Learn how the school of your dreams got started.</p>
+      </div>
+    </div>  
+  </div>
+<h1>This page is single.php</h1>
+  <div class="container container--narrow page-section">
+
+    <?php  
+
+    $theParent = wp_get_post_parent_id(get_the_ID());
+
+      if ($theParent) { ?>
+
+      <div class="metabox metabox--position-up metabox--with-home-link">
+      <p><a class="metabox__blog-home-link" href="<?php get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
+    </div>
+
+      <?php }
+     ?>
+
+   
+   <!-- 
+    <div class="page-links">
+      <h2 class="page-links__title"><a href="#">About Us</a></h2>
+      <ul class="min-list">
+        <li class="current_page_item"><a href="#">Our History</a></li>
+        <li><a href="#">Our Goals</a></li>
+      </ul>
+    </div>
+-->
+    <div class="generic-content">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
+    </div>
+
+  </div>
+
+
+
+<?php the_content(); ?>
+
+
+<?php }
+
+get_footer();
+?>
